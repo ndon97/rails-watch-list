8271c615fbefcfb27275a7b9652ca670fd900587
Nick Don <nick.don.1102@gmail.com>
2022-06-14 18:59:23 +1000

    Creating lists with movies, and bookmarks working

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,10 +1,12 @@
 class ListsController < ApplicationController
+  before_action :set_list, only: [:show, :destroy]
+
   def index
     @lists = List.all
   end
 
   def show
-    @list = List.find(params[:id])
+    @bookmark = Bookmark.new
   end
 
   def new
@@ -13,13 +15,24 @@ class ListsController < ApplicationController
 
   def create
     @list = List.new(list_params)
-    @list.save
-
-    # no need for app/views/restaurants/create.html.erb
-    redirect_to list_path(@list)
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
   end
 
+  def destroy
+    @list.destroy
+    redirect_to lists_path
+  end
+
+
   private
+
+  def set_list
+    @list = List.find(params[:id])
+  end
 
   def list_params
     params.require(:list).permit(:name)
